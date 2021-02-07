ba43cdacb16efbabb6f8e7495013393a04796a06
Graham Bohm <grahambohm@gmail.com>
2021-02-07 13:04:54 -0500

    Complete file upload for instructor and applicant data

--- server/app.js
+++ server/app.js
@@ -1,14 +1,95 @@
 const http = require("http");
+const express = require('express');
+const bodyParser = require("body-parser");
+const cors = require('cors');
+const multer = require('multer');
+const path = require('path');
 
 const hostname = "127.0.0.1";
-const port = 3000;
+const port = process.env.PORT || 5000;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    res.end("Hello World");
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+// var upload = multer({dest: 'temp/'});
+
+// const admin = require('firebase-admin');
+// admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount)
+//   });
+  
+// const db = admin.firestore();
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'temp/');
+    },
+
+    // By default, multer removes file extensions so let's add them back
+    filename: function(req, file, cb) {
+        cb(null, file.fieldname + '-' + 'temp' + path.extname(file.originalname));
+    }
 });
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+// Upload enpoint for all applicants data
+app.post('/api/uploadApplicantsFile', function (req, res) {
+    let upload = multer({ storage: storage, }).single('ApplicantsFile');
+
+    upload(req, res, function(err){
+        console.log(req.file);
+        res.status(200).send({data:"hell yeah"});
+    });
 });
+
+// Upload enpoint for all instructors data
+app.post('/api/uploadInstructorsFile', function (req, res) {
+    let upload = multer({ storage: storage, }).single('InstructorsFile');
+
+    upload(req, res, function(err){
+        console.log(req.file);
+        res.status(200).send({data:"hell yeah"});
+    });
+});
+
+// Get a schedule
+app.get('/api/profs',  (request, response) => {
+
+    try {
+        response.status(200).send({data:"something"});
+    }
+    catch(err){
+        response.send({
+            err: err.message,
+        });
+    }
+});
+
+// app.post('/api/uploadApplicantsFile',  (request, response) => {
+
+//     try {
+//         console.log(request.body);
+//         console.log(request.files);
+//         response.send({data:"something"});
+//     }
+//     catch(err){
+//         response.send({
+//             err: err.message,
+//         });
+//     }
+// });
+
+app.listen(port, hostname, () => {
+    console.log('Listening on: ' + port);
+});
+
+
+// const server = http.createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader("Content-Type", "text/plain");
+//     res.end("Hello World");
+// });
+
+// server.listen(port, hostname, () => {
+//     console.log(`Server running at http://${hostname}:${port}/`);
+// });
--- ta-match/src/App.js
+++ ta-match/src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const apiURL = 'http://localhost:5000/api';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      applicantsFile: null,
+      instructorsFile: null
+    }
+  }
+
+  onChangeHandler(event){
+    console.log(event.target.files[0]);
+    this.setState({
+      applicantsFile: event.target.files[0]
+    });
+  }
+
+  sendFile(filename){
+    console.log(filename);
+    const data = new FormData();
+    data.append(filename, this.state.applicantsFile);
+    axios.post(apiURL + '/upload' + filename, data, { 
+
+    }).then(res => {
+     console.log(res.statusText);
+   });
+  }
+
+  render() {
+    return (
+      <div className="App">
+          Choose Applicants File: <input
+            type="file"
+            id="applicantData"
+            accept=".xlsx, .xls, .csv"
+            onChange={this.onChangeHandler.bind(this)}
+          />
+        <Button className="submitButton"
+          variant="contained"
+          onClick={()=>this.sendFile.bind(this)('ApplicantsFile')}
+          >
+          Submit 
+        </Button>
+
+        Choose Instructors File: <input
+            type="file"
+            id="instructorsData"
+            accept=".xlsx, .xls, .csv"
+            onChange={this.onChangeHandler.bind(this)}
+          />
+        <Button className="submitButton"
+          variant="contained"
+          onClick={()=>this.sendFile.bind(this)('InstructorsFile')}
+          >
+          Submit 
+        </Button>
+      </div>
+    );
+  }
 }
-
-export default App;
+  export default App;
