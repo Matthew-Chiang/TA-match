6fcce7ac1a9321d98cb95231125565dd92aac218
Jenny Li <lijenny1999@gmail.com>
2021-02-09 17:31:48 -0500

    add dropdown

--- ta-match/src/components/SignIn.js
+++ ta-match/src/components/SignIn.js
@@ -3,8 +3,6 @@ import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
@@ -69,10 +67,6 @@ export default function SignIn() {
             id="password"
             autoComplete="current-password"
           />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          />
           <Button
             type="submit"
             fullWidth
@@ -82,14 +76,9 @@ export default function SignIn() {
           >
             Sign In
           </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2">
-                Forgot password?
-              </Link>
-            </Grid>
+          <Grid container justify="center">
             <Grid item>
-              <Link href="#" variant="body2">
+              <Link href="/signup" variant="body2">
                 {"Don't have an account? Sign Up"}
               </Link>
             </Grid>
--- ta-match/src/components/SignUp.js
+++ ta-match/src/components/SignUp.js
@@ -1,10 +1,8 @@
-import React, { useRef}  from 'react';
+import React, { useRef, useState}  from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import { useAuth } from '../contexts/AuthContext'
@@ -13,6 +11,11 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -37,17 +40,27 @@ const useStyles = makeStyles((theme) => ({
 
 export default function SignUp() {
   const classes = useStyles();
+
+  const [role, setRole] = useState('professor');
+
+  const handleChange = (event) => {
+    setRole(event.target.value);
+  };
+
   const { signup } = useAuth()
   const emailRef = useRef('')
   const passwordRef = useRef('')
   const fNameRef = useRef('')
   const lNameRef = useRef('')
+  const roleRef = useRef('')
 
-  function handleSubmit(a) {
-    a.preventDefault()
-    signup(emailRef.current.value, passwordRef.current.value)
-    console.log(fNameRef.current.value)
-    console.log(lNameRef.current.value)
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    if (emailRef.current.value == '')
+        alert('error')
+
+    // signup(emailRef.current.value, passwordRef.current.value)
   }
 
   return (
@@ -112,9 +125,25 @@ export default function SignUp() {
                 inputRef={passwordRef}
               />
             </Grid>
-            {/* <Grid item xs={12}>
-              
-            </Grid> */}
+            <Grid item xs={12}>
+            <TextField
+                id="role"
+                select
+                required
+                label="Role"
+                value={role}
+                onChange={handleChange}
+                SelectProps={{
+                    native: true,
+                }}
+                fullWidth
+                variant="outlined"
+                inputRef={roleRef}
+                >
+                <option value='professor'>Professor</option>
+                <option value='administrator'>Administrator</option>
+            </TextField>
+            </Grid>
           </Grid>
           <Button
             type="submit"
@@ -125,9 +154,9 @@ export default function SignUp() {
           >
             Sign Up
           </Button>
-          <Grid container justify="flex-end">
+          <Grid container justify="center">
             <Grid item>
-              <Link href="#" variant="body2">
+              <Link href="/login" variant="body2">
                 Already have an account? Sign in
               </Link>
             </Grid>
