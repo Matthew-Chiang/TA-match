40bf03efa2efce3a772dbb977334f6fa9d8a8ee1
josephmothauwo <jmotha@uwo.ca>
2021-02-11 15:37:57 -0500

    added card overview for each course

--- ta-match/src/components/CourseInfo.js
+++ ta-match/src/components/CourseInfo.js
@@ -3,12 +3,34 @@ import React, {Component} from 'react'
 import Accordion from '@material-ui/core/Accordion';
 import { AccordionDetails, AccordionSummary } from '@material-ui/core';
 import {Select, MenuItem, InputLabel, NativeSelect} from '@material-ui/core';
-import { getApplicantData } from '../services/ProfService'; 
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid'; 
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import { getApplicantData } from '../services/ProfService';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
-const url = "http://localhost:5000/api/"
+const styles = {
+    root: {
+      minWidth: 275,
+    },
+    bullet: {
+      display: 'inline-block',
+      margin: '0 2px',
+      transform: 'scale(0.8)',
+    },
+    title: {
+      fontSize: 14,
+    },
+    pos: {
+      marginTop: 6,
+    },
+  };
 
 
 class CourseInfo extends Component{
+    
     constructor(props) {
         super(props);
         this.state = {
@@ -29,47 +51,64 @@ class CourseInfo extends Component{
         })
     }
     render(){
+        const { classes } = this.props;
         const { isLoading, applicantData } = this.state;
         if (isLoading) {
         return <div className="App">Loading...</div>;
         }
             return (
-                <div>  
+                <div> 
+                <Grid container spacing={3}>
                 {applicantData["courseList"].map((course, index)=>{
-                    return <div>    
-                    <Accordion>
-                        <AccordionSummary>
+                    return <Grid item xs={12} sm={6} md={4}>
+
+                    <Card className={classes.root} variant="outlined">
+                        <CardContent>
+                            <Typography className={classes.title} color="textSecondary" gutterBottom>
+                            University of Western Ontario
+                            </Typography>
                             <p> {course["course_code"]} </p>
-                        </AccordionSummary>
-                        <AccordionDetails>
-                            <div>Applicants</div>
-                            <div>
-                                {course["applicant_list"].map((applicant,index)=>{
-                                    return <div>
-                                        <p>Name: {applicant.name}</p>
-                                        <p>email: {applicant.email}</p>
-                                        <p>fundable: {applicant.fundable}</p>
-                                        <p>answer1: {applicant.answer1}</p>
-                                        <p>answer2: {applicant.answer2}</p>
-                                        <InputLabel id="label">Rank</InputLabel>
-                                        <NativeSelect id="select">
-                                            <option value="1">1</option>
-                                            <option value="2">2</option>
-                                            <option value="3">3</option>
-                                            <option value="4">4</option>
-                                        </NativeSelect>
+                            <Typography className={classes.pos} color="textSecondary">
+                            Winter 2021
+                            </Typography>
+                            <Accordion>
+                            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                            <div>Applicants:</div>
+                            </AccordionSummary>
+                                <AccordionDetails>
+                                    <div>
+                                        {course["applicant_list"].map((applicant,index)=>{
+                                            return <div>
+                                                {/* need to be dynamic */}
+                                                <p>Name: {applicant.name}</p>
+                                                <p>Email: {applicant.email}</p>
+                                                <p>Fundable: {applicant.fundable}</p>
+                                                <p>Answer 1: {applicant.answer1}</p>
+                                                <p>Answer 2: {applicant.answer2}</p>
+                                                <InputLabel id="label">Rank</InputLabel>
+                                                <NativeSelect id="select">
+                                                {course["applicant_list"].map((applicant, index) => {
+                                                    return <option>{index + 1}</option>;
+                                                })}
+                                                </NativeSelect>
+                                            </div>
+                                        })}
                                     </div>
-                                })}
-                            </div>
-                        </AccordionDetails>
-                    </Accordion>
-                    </div>
-                    // return <h1>{course.course}</h1>
+                                </AccordionDetails>
+                        </Accordion>
+                        </CardContent>
+                    </Card>
+
+
+                        
+                    </Grid> 
                 })}
-            </div>
+                </Grid> 
+                </div>
+            
             );
     }
 }
 
 
-export default CourseInfo;
+export default withStyles(styles)(CourseInfo)
