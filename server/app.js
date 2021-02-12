5de723c37d12ee4c31915f1688404ba8ce882322
Saeptum <45183693+Saeptum@users.noreply.github.com>
2021-02-11 19:36:55 -0500

    Export professor questions into an excel file

--- server/app.js
+++ server/app.js
@@ -1,14 +1,59 @@
 const http = require("http");
+const xlsx = require("xlsx");
+const express = require('express');
 
-const hostname = "127.0.0.1";
-const port = 3000;
+const hostname = "localhost";
+const port = 4200;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    res.end("Hello World");
+//const server = http.createServer((req, res) => {
+//    res.statusCode = 200;
+//    res.setHeader("Content-Type", "text/plain");
+//    res.end("Hello World");
+//}); 
+
+const app = express();
+const cors = require('cors');
+const admin = require('firebase-admin');
+const serviceAccount = require('./ta-match-key.json');
+
+app.use(cors());
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
 });
+const db = admin.firestore();
 
-server.listen(port, hostname, () => {
+app.get('/api/questions/:semester', async (req, res) => {
+    const semester = req.params.semester;
+    let response = [];
+    const viewers = [
+        {course: 1, question1: '1', question2: '2'},
+        {course: 2, question1: '1', question2: '2', question3: '3'}
+    ]
+    try {
+        const questionsCollection = await db.collection('courses').doc(semester).collection('courses').get();
+        let fields = [];
+        questionsCollection.forEach(doc => {
+            fields = Object.keys(doc.data());
+            let questions = doc.data();
+            console.log(questions);
+            questionsWithCourseID = Object.assign({course: doc.id}, questions); //puts it into required format
+            console.log(questionsWithCourseID);
+
+            //console.log(fields.length);
+            //console.log(fields);
+            //questionsArray.push(doc.data());
+            //response.push({course: doc.id, questions: questionsArray});
+            response.push(questionsWithCourseID);
+            //questionsArray.push(doc.data());
+        })
+        res.json(response);
+    }catch(err) {
+        console.log(err);
+    }
+
+})
+
+app.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
--- ta-match/src/App.js
+++ ta-match/src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
+import TextField from '@material-ui/core/TextField';
 import './App.css';
+import React from 'react';
+import {ExportCSV} from './ExportCSV';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  class Questions extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        input: '',
+        error: null,
+        isLoaded: false,
+        items: []
+      }
+      this.handleChange = this.handleChange.bind(this);
+    }
 
-export default App;
+    handleExport = () => {
+      console.log(this.state.input);
+      fetch(`http://localhost:4200/api/questions/${this.state.input}`)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result
+          })
+          console.log(this.state.items);
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          })
+        }
+      )
+    }
+
+    handleChange(event) {
+      this.setState({input: event.target.value});
+    }
+
+    render() {
+      const {error, isLoaded, items} = this.state;
+        const fileName = "Questions";
+        return (
+          <div>
+            <h1>Export Questions</h1>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="semester"
+              label="Semester"
+              name="semester"
+              value={this.state.input}
+              onChange={this.handleChange}
+            />
+            <button onClick={this.handleExport} noValidate>Download Questions for Export</button>
+
+            <ExportCSV csvData={this.state.items} fileName={fileName} />
+
+            <ul>
+              {items.map(item => (
+                <li key={item.course}>
+                  <p><b>{item.course + ": "}</b></p>
+                  <p>{item.question1}</p>
+                  <p>{item.question2}</p>
+                  <p>{item.question3}</p>
+                </li>
+              ))}
+            </ul>
+            
+          </div>
+        )      
+    }
+  }
+export default Questions;
