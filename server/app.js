be15fb842484ac078e1df72806c87fb91f81bcf6
Saeptum <45183693+Saeptum@users.noreply.github.com>
2021-03-16 23:38:07 -0400

    Changed semester input to dropdown menu

--- server/app.js
+++ server/app.js
@@ -430,16 +430,27 @@ app.delete("/api/allocation/delete", async (req, res) => {
 app.get("/api/semester/:semester", async (req, res) => {
     const semester = req.params.semester;
     let response = [];
+    let courseIDs = [];
+    let courseData = [];
+
     const coursesCollection = await db.collection('courses').doc(semester).collection('courses').get();
     coursesCollection.forEach(doc => {
-        console.log(doc.id, '=>', doc.data())
-
-        let courseDetails = {
-            course: doc.id,
-            details: doc.data(),
-        };
-        response.push(courseDetails);
+        courseIDs.push(doc.id);
+        courseData.push(doc.data());
     })
+    for(let i=0; i<courseIDs.length; i++) {
+        let courseTAs = [];
+        const allocationsCollection = await db.collection('courses').doc(semester).collection('courses').doc(courseIDs[i]).collection('allocation').get();
+        allocationsCollection.forEach(ta => {
+            courseTAs.push(ta.id);
+        })
+        let courseDetails = {
+            course: courseIDs[i],
+            details: courseData[i],
+            allocation: courseTAs,
+        }
+        response.push(courseDetails);
+    }
     if(response.length != 0) {
         res.json(response);
     }else {
--- ta-match/src/components/HistoricalData.js
+++ ta-match/src/components/HistoricalData.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button, TextField, IconButton } from '@material-ui/core';
+import {IconButton } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
 import axios from 'axios';
 
@@ -20,11 +20,23 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import Box from '@material-ui/core/Box';
 
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+
 const apiURL = 'http://localhost:5000/api';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 120,
+    },
+    searchIcon: {
+        margin: theme.spacing(2.5),
+    },
 
-});
+  }));
 
 const StyledTableCell = withStyles((theme) => ({
     head: {
@@ -35,42 +47,56 @@ const StyledTableCell = withStyles((theme) => ({
   }))(TableCell);
 
 export default function HistoricalData() {
+    const classes = useStyles();
     const [semester, setSemester] = useState('');
-    const [course, setCourse] = useState('');
+    const [year, setYear] = useState('');
     const [semesterInfo, setSemesterInfo] = useState([]);
     const [applicants, setApplicants] = useState([]);
+    const [allocations, setAllocations] = useState([]);
     const [open, setOpen] = useState(false);
 
-    const handleInputChange = (event) => {
+    const handleSemester = (event) => {
         const updatedSemester = event.target.value;
         setSemester(updatedSemester);
     }
 
+    const handleYear = (event) => {
+        const updatedYear = event.target.value;
+        setYear(updatedYear);
+    }
+
     const getSemesterInfo = () => {
         let response = [];
-        axios.get(apiURL + `/semester/${semester}`)
+        let semesterAndYear = semester + year;
+        axios.get(apiURL + `/semester/${semesterAndYear}`)
         .then(res => {
             res.data.forEach(course => {
                 response.push(course);
             })
-            //console.log(res.data);
             setSemesterInfo(response);
         })
         .catch(err => {
             console.log(err);
             window.alert("That semester does not exist!");
+            setSemesterInfo([]);
+            setApplicants([]);
+            setAllocations([]);
         });
     }
 
     const getApplicants = (course) => {
         let response = [];
-        axios.get(apiURL + `/applicants/${semester}/${course}`)
+        let semesterAndYear = semester + year;
+        axios.get(apiURL + `/applicants/${semesterAndYear}/${course}`)
         .then(res => {
             res.data.forEach(applicant => {
                 response.push(applicant);
             })
-            //console.log(res.data);
+            console.log(res.data);
             setApplicants(response);
+        })
+        .catch(err => {
+            console.log(err);
         })
     }
 
@@ -79,14 +105,31 @@ export default function HistoricalData() {
     }
 
     const handleClose = () => {
+        setApplicants([]);
         setOpen(false);
     }
-
     return(
         <div>
             <h1>View Past Semesters</h1>
-            <TextField id="outlined-basic" label="Semester" variant="outlined" value={semester} onChange={handleInputChange}/>
-            <IconButton color="primary" onClick={getSemesterInfo}>
+            <FormControl className={classes.formControl}>
+                <InputLabel>Semester</InputLabel>
+                <Select value={semester} onChange={handleSemester}>
+                    <MenuItem value="summer">Summer</MenuItem>
+                    <MenuItem value="fall">Fall</MenuItem>
+                    <MenuItem value="winter">Winter</MenuItem>
+                </Select>
+            </FormControl>
+            <FormControl className={classes.formControl}>
+                <InputLabel>Year</InputLabel>
+                <Select value={year} onChange={handleYear}>
+                    <MenuItem value="2021">2021</MenuItem>
+                    <MenuItem value="2020">2020</MenuItem>
+                    <MenuItem value="2019">2019</MenuItem>
+                    <MenuItem value="2018">2018</MenuItem>
+                    <MenuItem value="2017">2017</MenuItem>
+                </Select>
+            </FormControl>
+            <IconButton className={classes.searchIcon} color="primary" onClick={getSemesterInfo}>
                 <SearchIcon />
             </IconButton>
             <br /><br /><br />
@@ -97,6 +140,7 @@ export default function HistoricalData() {
                             {semesterInfo.length > 0 && <StyledTableCell>COURSE</StyledTableCell>}
                             {semesterInfo.length > 0 && <StyledTableCell>INSTRUCTOR</StyledTableCell>}
                             {semesterInfo.length > 0 && <StyledTableCell>TA HOURS</StyledTableCell>}
+                            {semesterInfo.length > 0 && <StyledTableCell>TAs</StyledTableCell>}
                             {semesterInfo.length > 0 && <StyledTableCell>APPLICANTS</StyledTableCell>}
                         </TableRow>
                     </TableHead>
@@ -107,7 +151,14 @@ export default function HistoricalData() {
                                     <TableRow key={course.course}>
                                         <TableCell>{course.course}</TableCell>
                                         <TableCell>{course.details.instructor}</TableCell>
-                                        <TableCell>{course.details.ta_hours}</TableCell>
+                                        <TableCell> {course.details.ta_hours}</TableCell>
+                                        <TableCell>{
+                                            course.allocation.map(ta => {
+                                                return(
+                                                    <li style={{listStyleType: 'none'}}key={ta}>{ta}</li>
+                                                )
+                                            })
+                                        }</TableCell>
                                         <TableCell><IconButton onClick={() => { getApplicants(course.course); handleOpen() }}><AccountCircleIcon /></IconButton></TableCell>
                                     </TableRow>
                                 )
@@ -120,7 +171,6 @@ export default function HistoricalData() {
         </div>
     )
 }
-
 function ApplicantsDialog(props) {
     const classes = useStyles();
     const {open, close, applicants} = props;
