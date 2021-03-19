5a30bd92ee94ec957d990db2b0678c83270c840d
Matthew <matthew.chiang.myc@gmail.com>
2021-03-18 23:26:19 -0400

    changed both dialogs on prof page to prettier version

--- ta-match/src/components/QuestionAnswerModal.js
+++ ta-match/src/components/QuestionAnswerModal.js
@@ -1,7 +1,11 @@
 import Dashboard from "../components/Dashboard";
 import "../App.css";
-import Modal from "@material-ui/core/Modal";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogTitle from "@material-ui/core/DialogTitle";
 import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import CourseInfo from "../components/CourseInfo";
 
 import Button from "@material-ui/core/Button";
@@ -12,6 +16,29 @@ const QuestionAnswerModal = ({ questionAnswers }) => {
     const [openQuestionAnswerModal, setOpenQuestionAnswerModal] = useState(
         false
     );
+
+    const useStyles = makeStyles((theme) => ({
+        btn: {
+            float: "right",
+            marginTop: 20,
+        },
+        dialogTitle: {
+            textAlign: "center",
+            marginLeft: "auto",
+            marginRight: "auto",
+            width: "100%",
+        },
+        dialogContainer: {
+            paddingLeft: "70px",
+            paddingBottom: "100px",
+        },
+        questionPair: {
+            paddingBottom: "30px",
+        },
+    }));
+
+    const classes = useStyles();
+
     return (
         <div>
             <Button
@@ -24,25 +51,47 @@ const QuestionAnswerModal = ({ questionAnswers }) => {
                 See Full Application
             </Button>
 
-            <Modal
+            <Dialog
                 open={openQuestionAnswerModal}
                 onClose={() => {
                     setOpenQuestionAnswerModal(false);
                 }}
+                fullWidth={true}
+                maxWidth="md"
             >
+                <DialogActions>
+                    <Button
+                        onClick={() => {
+                            setOpenQuestionAnswerModal(false);
+                        }}
+                        color="primary"
+                    >
+                        Close
+                    </Button>
+                </DialogActions>
                 <div style={{ backgroundColor: "white" }}>
-                    {questionAnswers.map((QaPairs, index) => {
-                        return (
-                            <div key={index}>
-                                <Typography variant="h6">
-                                    Question {index + 1}: {QaPairs.question}
-                                </Typography>
-                                <Typography>{QaPairs.answer}</Typography>
-                            </div>
-                        );
-                    })}
+                    <DialogTitle>
+                        <h1 className={classes.dialogTitle}>
+                            View TA Application
+                        </h1>
+                    </DialogTitle>
+                    <DialogContent className={classes.dialogContainer}>
+                        {questionAnswers.map((QaPairs, index) => {
+                            return (
+                                <div
+                                    key={index}
+                                    className={classes.questionPair}
+                                >
+                                    <Typography variant="h6">
+                                        Question {index + 1}: {QaPairs.question}
+                                    </Typography>
+                                    <Typography>{QaPairs.answer}</Typography>
+                                </div>
+                            );
+                        })}
+                    </DialogContent>
                 </div>
-            </Modal>
+            </Dialog>
         </div>
     );
 };
